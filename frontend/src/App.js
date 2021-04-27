d8dfc1d68487fcf84270cd8d5a27b832577ad2d1
Karan Bains <karanbains.bains@gmail.com>
2021-04-28 02:42:54 +0530

    Refractored the folder structure and document upload page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,45 +2,65 @@ import React from "react";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { Route, Switch } from "react-router-dom";
-import Login from "./components/Auth/Login/Login";
-import Email from "./components/Auth/Signup/Email";
-import Home from "./components/Home/Home";
-import Ride from "./components/Ride/Ride";
-import Footer from "./components/Footer/Footer";
-import Reserve from "./components/Reserve/Reserve";
-import Contact from "./components/Contact/Contact";
-import Otp from "./components/Auth/Signup/Otp";
-import Signup from "./components/Auth/Signup/Signup";
-import MyRide from "./components/MyRide/MyRide";
-import License from './components/Auth/Signup/License/License';
+
+//Shared Imports
+import Login from "./components/Shared/Login/Login";
+import NavbarComponent from './components/Shared/Navbar/NavbarComponent';
+import Footer from "./components/Shared/Footer/Footer";
+
+//Client Imports
+import ClientEmail from "./components/Client/Client-Signup/Email";
+import ClientHome from "./components/Client/Client-Home/Home";
+import ClientRide from "./components/Client/Client-Ride/Ride";
+import ClientReserve from "./components/Client/Client-Reserve/Reserve";
+import ClientContact from "./components/Client/Client-Contact/Contact";
+import ClientOtp from "./components/Client/Client-Signup/Otp";
+import ClientSignup from "./components/Client/Client-Signup/Signup";
+import ClientMyRide from "./components/Client/Client-MyRide/MyRide";
+import ClientVerify from './components/Client/Client-Signup/Client-Verify/Verify';
+
+//Driver Imports
+import DriverSignup from "./components/Driver/Driver-Signup/Signup";
+import DriverOtp from "./components/Driver/Driver-Signup/Otp";
+import DriverEmail from "./components/Driver/Driver-Signup/Email";
+import DriverVerify from './components/Driver/Driver-Signup/Driver-Verify/Verify';
+import DriverAddVehicle from './components/Driver/Driver-Signup/AddVehicle';
 
 function App() {
   return (
     <div className="App">
+      <NavbarComponent />
       <Switch>
         {/* Home */}
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={ClientHome} />
 
         {/* Login */}
         <Route exact path="/login" component={Login} />
 
-        {/* Sign up */}
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/otp" component={Otp} />
-        <Route exact path="/email" component={Email} />
-        <Route exact path="/license" component={License} />
+        {/*  Client Sign up */}
+        <Route exact path="/client/signup" component={ClientSignup} />
+        <Route exact path="/client/otp" component={ClientOtp} />
+        <Route exact path="/client/email" component={ClientEmail} />
+        <Route exact path="/client/verify" component={ClientVerify} />
+
+        {/* Driver Sign up */}
+        <Route exact path="/driver/signup" component={DriverSignup} />
+        <Route exact path="/driver/otp" component={DriverOtp} />
+        <Route exact path="/driver/email" component={DriverEmail} />
+        <Route exact path="/driver/verify" component={DriverVerify} />
+        <Route exact path="/driver/add-vehicle" component={DriverAddVehicle} />
 
         {/* Contact */}
-        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/client/contact" component={ClientContact} />
 
         {/* Ride */}
-        <Route exact path="/ride" component={Ride} />
+        <Route exact path="/client/ride" component={ClientRide} />
 
          {/* My Ride */}
-         <Route exact path="/myride" component={MyRide} />
+         <Route exact path="/client/myride" component={ClientMyRide} />
 
         {/* Reserve */}
-        <Route path="/reserve" component={Reserve} />
+        <Route path="/client/reserve" component={ClientReserve} />
       </Switch>
       <Footer />
     </div>
---
+++ frontend/src/components/Client/Client-Home/Work/Work.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import customerOne from "../../../../images/customerOne.svg";
+import customerTwo from "../../../../images/customerTwo.png";
+import customerThree from "../../../../images/customerThree.png";
+import customerFour from "../../../../images/customerFour.png";
+
+function Work() {
+  return (
+    <>
+      <div className="">
+        <div className="container py-section px-4">
+          <h2 className="text-primaryColor font-bold sub-heading text-center">
+            Our Happy Customers
+          </h2>
+          <div className="row">
+            <div className="col-lg-3 col-md-3 col-sm-6 col-12 px-5 center">
+              <img
+                src={customerOne}
+                alt="customer"
+                className="img-fluid px-5 py-5"
+              />
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-6 col-12 px-5 center">
+              <img
+                src={customerTwo}
+                alt="customer"
+                className="img-fluid px-5 py-5"
+              />
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-6 col-12 px-5 center">
+              <img
+                src={customerThree}
+                alt="customer"
+                className="img-fluid px-5 py-5"
+              />
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-6 col-12 px-5 center">
+              <img
+                src={customerFour}
+                alt="customer"
+                className="img-fluid px-5 py-5"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="container px-4 mb-5">
+          <h2 className="text-primaryColor font-bold sub-heading text-center mb-lg-5 mb-0">
+            How it works
+          </h2>
+          <div className="row how-it-works pt-5">
+            <div className="col-lg-4 mt-2 col-md-4 col-sm-12 col-12">
+              <div className="d-flex justify-content-center">
+                <div
+                  className="work-number text-primaryColor font-bold center"
+                  style={{ fontSize: "35px" }}
+                >
+                  1
+                </div>
+              </div>
+              <h4 className="text-primaryColor font-20 mt-5 font-bold text-center">
+                Select a type of copy
+              </h4>
+              <p className="text-muted text-center font-demi mb-3 mt-2 font-16">
+                Choose from headlines, blog intros, product descriptions, and
+                much more.
+              </p>
+            </div>
+            <div className="col-lg-4 mt-2 col-md-4 col-sm-12 col-12">
+              <div className="d-flex justify-content-center">
+                <div
+                  className="work-number text-primaryColor font-bold center"
+                  style={{ fontSize: "35px" }}
+                >
+                  2
+                </div>
+              </div>
+              <h4 className="text-primaryColor font-20 mt-5 font-bold text-center">
+                Describe your product
+              </h4>
+              <p className="text-muted text-center font-demi mb-3 mt-2 font-16">
+                Just enter the name of your company and 1-2 sentences on what
+                you do.
+              </p>
+            </div>
+            <div className="col-lg-4 mt-2 col-md-4 col-sm-12 col-12">
+              <div className="d-flex justify-content-center">
+                <div
+                  className="work-number text-primaryColor font-bold center"
+                  style={{ fontSize: "35px" }}
+                >
+                  3
+                </div>
+              </div>
+              <h4 className="text-primaryColor font-20 mt-5 font-bold text-center">
+                Get your results and edit
+              </h4>
+              <p className="text-muted text-center font-demi mb-3 mt-2 font-16">
+                You’ll get 10 results at a time, and you can run the tool again
+                for more ideas.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Work;
